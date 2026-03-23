7caf878bb7dd960d4e880db7127f2b29e65fca6c
Kamila Pineda Escobar <kamimimi@MacBook-Pro-de-Kamila.local>
2026-03-23 13:09:11 -0600

    El punto 2: Keypoints using MP Holistic debe funcionar hasta aquí sin ningún problema

--- vision_test.py
+++ vision_test.py
@@ -24,6 +24,28 @@ def draw_landmarks(image, results):
     if results.right_hand_landmarks:
         mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
+                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                             ) 
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             ) 
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             ) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             )
+
+
 # 2. Loop principal
 cap = cv2.VideoCapture(0)
 
@@ -34,15 +56,33 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         if not ret: break
 
         image, results = mediapipe_detection(frame, holistic)
-        draw_landmarks(image, results)
+        print(results)
 
-        cv2.imshow('Hi-5 KABAL - Vision Test', image)
+        #draw landmarks
+        draw_styled_landmarks(image, results)
+
+        cv2.imshow('OpenCV Feed', image)
+
+        #if results.face_landmarks:
+            # Imprime solo los primeros 5 puntos para no saturar la consola
+            #print(results.face_landmarks.landmark[:5]) 
+        #else:
+            #print("No se detecta rostro")
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
-    # Limpieza necesaria para tu M4 Pro
+
+    
+
+    #lIMPIAR agregado para mac
     for i in range(1, 10):
         cv2.waitKey(1)
+
+##EXTRACT KEYPOINTS VALUES
+pose = []
+for res in results.pose_landmarks.landmark:
+    test = np.array([res.x, res.y, res.z, res.visibility])
+    pose.append(test)
